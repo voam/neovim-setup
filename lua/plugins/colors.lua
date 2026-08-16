76b83021b84a5918e3ee03444a1ee8d5c555f52b
Tony O'Rourke <sonicwrx@gmail.com>
2026-08-16 00:19:24 -0400

    add in multiple themes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,28 +1,37 @@
 return {
-  'maxmx03/solarized.nvim',
-  lazy = false,
-  priority = 1000,
-  ---@type solarized.config
-  opts = { transparency = { enabled = true } },
-  config = function(_, opts)
-    vim.o.termguicolors = true
-    vim.o.background = 'dark'
-    require('solarized').setup(opts)
-    vim.cmd.colorscheme 'solarized'
-end,
+    {
+        'maxmx03/solarized.nvim',
+        lazy = true,
+        priority = 1000,
+        ---@type solarized.config
+        opts = { transparency = { enabled = true },
+                variant = 'summer'
+            },
+        config = function(_, opts)
+            vim.o.termguicolors = true
+            vim.o.background = 'dark'
+            require('solarized').setup(opts)
+            vim.cmd.colorscheme 'solarized'
+        end,
+    },
+
+    { "folke/tokyonight.nvim", lazy = false },
+
+    -- Theme 3: Kanagawa (Installed and available)
+    { "rebelot/kanagawa.nvim", lazy = true },
 }
 
 -- local function enable_transparency()
---     vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
--- end
--- return {
---   -- the colorscheme should be available when starting Neovim
---   {
---     "folke/tokyonight.nvim",
---     config = function()
---       -- load the colorscheme here
---       vim.cmd([[colorscheme tokyonight]])
---       enable_transparency()
---     end
---   },
--- }
+    --     vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+    -- end
+    -- return {
+        --   -- the colorscheme should be available when starting Neovim
+        --   {
+            --     "folke/tokyonight.nvim",
+            --     config = function()
+                --       -- load the colorscheme here
+                --       vim.cmd([[colorscheme tokyonight]])
+                --       enable_transparency()
+                --     end
+                --   },
+                -- }
