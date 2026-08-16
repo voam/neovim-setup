fda081a5b1535049813731bf8c5ec31097222ad3
Tony O'Rourke <sonicwrx@gmail.com>
2026-08-15 20:17:07 -0400

    use latest version of telescope.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim', branch = 'master',
+    --'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function() 
 	local builtin = require('telescope.builtin')
